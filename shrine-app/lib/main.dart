636964d05f89dbc4a3876a988e7d3c708a844e2d
Jordon Douglas <jordondouglas@Jordons-MacBook-Air.local>
2025-05-13 17:34:08 -0500

    added firebase files to gitignore

--- shrine-app/lib/main.dart
+++ shrine-app/lib/main.dart
@@ -12,8 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shrine/firebase_options.dart';
 
 import 'app.dart';
 
-void main() => runApp(const ShrineApp());
+//void main() => runApp(const ShrineApp());
+//Initilaizes Firebase 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ShrineApp());
+}
